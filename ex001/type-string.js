841f11a80bb1cd868ee51593611375d4e0ba9713
Italo61-dev <italosousa42541@gmail.com>
2023-08-02 20:59:02 -0300

    Create type-string.js

---
+++ ex001/type-string.js
@@ -0,0 +1,11 @@
+const texto1 = "Olá, mundo!";
+const textto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!"
+const StringDeNumeros = "12345";
+
+//concatenação (+)
+const citacao = 'Meu nome é ';
+const meuNome = "Leonardo"
+console.log(citacao + meuNome)
+
+//template string OU template Literal
