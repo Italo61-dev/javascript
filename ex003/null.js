3d49216c5bba0c43b069a4980121c9a2b506d600
Italo61-dev <italosousa42541@gmail.com>
2023-08-03 21:49:15 -0300

    atualizei

---
+++ ex003/null.js
@@ -0,0 +1,15 @@
+// undefined
+
+// null ==> vazio ou nada
+
+let minhVar;
+let varNull = null;
+
+console.log(minhVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero)
+console.log(typeof texto)
--- ex003/truthy-falsy.js
+++ ex003/truthy-falsy.js
@@ -1,20 +1,10 @@
-// boolean
+//truthy ou falsy
 
-const usuarioLogado = true;
-const contaPaga = false; 
+//0 => false
+//1 => true
 
-// truthy ou falsy
-
-// 0 => false
-// 1 => true
-
-// console.log(0 == false)
-// console.log("" == false)
-// console.log(1 == true)
-
-// undefined
-
-// null ==> vazio ou nada
-
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
 
 
