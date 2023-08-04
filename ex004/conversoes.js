52dfc0fe8f8b32cd73655975076ebf5023ccf852
Italo61-dev <italosousa42541@gmail.com>
2023-08-03 22:19:36 -0300

    Create conversoes.js

---
+++ ex004/conversoes.js
@@ -0,0 +1,17 @@
+// tipo de dado 
+//boleanos
+
+//conversão implicita
+    // conventeu as duas const para string
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + numeroString)
+
+//conversão implícita
+    //Number() => se preciso converter para numero
+    //String() => se preciso converter para caracter
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + Number(numeroString))
