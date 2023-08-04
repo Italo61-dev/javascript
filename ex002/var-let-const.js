10301a9053528b77ad87de52e43192b8a3b82c00
Italo61-dev <italosousa42541@gmail.com>
2023-08-03 21:10:04 -0300

    Update var-let-const.js

--- ex002/var-let-const.js
+++ ex002/var-let-const.js
@@ -1,4 +1,40 @@
 //var
 
-var altura = 5;
-var comprimento = 7;
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area);
+//var area;
+
+//let
+
+//let altura = 5;
+//let comprimento = 7;
+//let forma = "retangulo"
+
+//let area;
+
+//if (forma === "retangulo"){
+//    area = altura * comprimento;
+//} else {
+//    area = (altura * comprimento) / 2;
+//}
+
+//console.log(area)
+
+
+//const
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === "triângulo"){
+    area = altura * comprimento;
+} else {
+    area = (area * comprimento) / 2;
+}
+
+console.log(area)
