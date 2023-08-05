8058e1491c1fd3e21f17d06aeaed62fd4f55ff1d
Italo61-dev <italosousa42541@gmail.com>
2023-08-04 22:56:24 -0300

    criei ex010

---
+++ ex010/template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Italo"
+const idade = 2023-1999
+const cidadeDeNascimento = "Brasília"
+
+//const apresentação = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na ciade de " + cidadeDeNascimento;
+
+const apresentaçao = `Meu nome é ${nome}, minha idade é ${idade}, e nasci na cidade ${cidadeDeNascimento}.`
+
+console.log(apresentaçao)
