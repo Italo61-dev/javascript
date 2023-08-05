c8b44ffad6a21bb175d12669e7c5c8b1d3a5107f
Italo61-dev <italosousa42541@gmail.com>
2023-08-04 22:01:31 -0300

    criei ex007

---
+++ ex007/comparacoes.js
@@ -0,0 +1,18 @@
+// comparação implícita
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto)
+
+//typeof 
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == só compara o valor
+// == comparar o valor eo tipo de dado
+
+// conversão explicita
+
+//Number()
+//String()
