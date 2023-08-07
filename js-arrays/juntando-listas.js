0b7aac46471f37cc86224afaba218967ff839bc4
Italo61-dev <italosousa42541@gmail.com>
2023-08-07 20:08:35 -0300

    Create juntando-listas.js

---
+++ js-arrays/juntando-listas.js
@@ -0,0 +1,8 @@
+// DESAFIO - Jutando as salas -> Haverá uma palestra sobre padrões de projetos para as alunas das salas de JS e Python. Junte amabas as salas em uma única lista que exiba todas as alunas.
+
+const salaJs = ["Evaldo ", "Camis ", "Mari " ];
+const salaPython = ["Ju ", "Leo ", "Raquel"];
+
+const salasUnificadas = salaJs.concat(salaPython)
+
+console.log(`Relação a seguir de quem irá assistir a palestra: ${salasUnificadas}.`)
