abc47c07a59742754a52fd0c3a05586fb9514493
Italo61-dev <italosousa42541@gmail.com>
2023-08-07 20:51:25 -0300

    Update lista-duas-dimensoes.js

--- js-arrays/lista-duas-dimensoes.js
+++ js-arrays/lista-duas-dimensoes.js
@@ -1,3 +1,4 @@
+// Desafio -> de a nota para cada um dos alunos
 const alunos = ["João", "Juliana", "Ana", "Caio"];
 const medias = [10, 8, 7.5, 9];
 
@@ -7,3 +8,8 @@ console.log(`O aluno da posição 0 da lista de alunos é: ${listaDeAlunosEMedia
 console.log(`A aluna da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]}. E sua nota é ${listaDeAlunosEMedias[1][1]}.`)
 console.log(`A aluna da posição 2 da lista de alunos é: ${listaDeAlunosEMedias[0][2]}. E sua nota é ${listaDeAlunosEMedias[1][2]}.`)
 console.log(`O aluno da posição 3 da lista de alunos é: ${listaDeAlunosEMedias[0][3]}. E sua nota é ${listaDeAlunosEMedias[1][3]}.`)
+
+//Com base nessa matriz, qual dos códigos abaixo eu posso utilizar para imprimir a frase “Leonardo tem idade 28”?
+const funcionarios = [["Ana", "Juliana", "Leonardo"],[30, 35, 28]];
+
+console.log(`${funcionarios[0][2]} tem idade ${funcionarios[1][2]}`)
