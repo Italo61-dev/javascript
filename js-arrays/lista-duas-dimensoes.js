a8cd34e0b33ac3d81cff1475b19cfe4f41186483
Italo61-dev <italosousa42541@gmail.com>
2023-08-07 20:38:00 -0300

    Create lista-duas-dimensoes.js

---
+++ js-arrays/lista-duas-dimensoes.js
@@ -0,0 +1,9 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`O aluno da posição 0 da lista de alunos é: ${listaDeAlunosEMedias[0][0]}. E sua nota é ${listaDeAlunosEMedias[1][0]}.`)
+console.log(`A aluna da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]}. E sua nota é ${listaDeAlunosEMedias[1][1]}.`)
+console.log(`A aluna da posição 2 da lista de alunos é: ${listaDeAlunosEMedias[0][2]}. E sua nota é ${listaDeAlunosEMedias[1][2]}.`)
+console.log(`O aluno da posição 3 da lista de alunos é: ${listaDeAlunosEMedias[0][3]}. E sua nota é ${listaDeAlunosEMedias[1][3]}.`)
