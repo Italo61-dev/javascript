1b4bcd8f152f717a0fb85dcb0d8ed5260bc9e1a7
Italo61-dev <italosousa42541@gmail.com>
2023-08-08 11:51:48 -0300

    Update for.js

--- js-arrays/for.js
+++ js-arrays/for.js
@@ -1,7 +1,12 @@
 //Imprima o índice e a lista com os seguintes números: 100, 200, 300, 400, 500, 600
 
+
+//Primeria expressão: é executada apenas uma única vez
+//Segunda expressão: condição de execução
+//Terceira expressão: é executada sempre ao final do bloco
+
 const numeros = [100, 200, 300, 400, 500, 600];
 
-for (let indice = 0; indice < 6; indice = indice + 1) {
-
+for (let indice = 0; indice < numeros.length; indice ++) {
+    console.log(numeros[indice])
 }
