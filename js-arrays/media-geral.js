67eaaafd09dbdf54830b43d68fb268950950ce2d
Italo61-dev <italosousa42541@gmail.com>
2023-08-16 21:01:40 -0300

    Create media-geral.js

---
+++ js-arrays/media-geral.js
@@ -0,0 +1,17 @@
+ const salaJs = [7, 8, 8, 7, 10, 6.5, 4, 10, 7]
+ const salaJava = [6, 5, 8, 9, 5, 6]
+ const salaPython = [7, 3.5, 8, 9.5]
+
+ function calculaMedia(notasDaSala) {
+    const somaDasnotas = notasDaSala.reduce((acumulador, nota) => {
+        return acumulador + nota;
+    }, 0);
+
+    const media = somaDasnotas / notasDaSala.length;
+
+    return media;
+ }
+
+ console.log(`A média da sala de JS é ${calculaMedia(salaJs)}`)
+ console.log(`A média da sala de Java é ${calculaMedia(salaJs)}`)
+ console.log(`A média da sala de Python é ${calculaMedia(salaJs)}`)
