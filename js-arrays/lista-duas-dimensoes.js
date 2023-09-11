01c4b2e17910a636a6d58f3b50f94397388d85ee
Italo61-dev <italosousa42541@gmail.com>
2023-09-11 20:17:08 -0300

    Update lista-duas-dimensoes.js

--- js-arrays/lista-duas-dimensoes.js
+++ js-arrays/lista-duas-dimensoes.js
@@ -4,10 +4,10 @@ const medias = [10, 8, 7.5, 9];
 
 const listaDeAlunosEMedias = [alunos, medias];
 
-console.log(`O aluno da posição 0 da lista de alunos é: ${listaDeAlunosEMedias[0][0]}. E sua nota é ${listaDeAlunosEMedias[1][0]}.`)
-console.log(`A aluna da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]}. E sua nota é ${listaDeAlunosEMedias[1][1]}.`)
-console.log(`A aluna da posição 2 da lista de alunos é: ${listaDeAlunosEMedias[0][2]}. E sua nota é ${listaDeAlunosEMedias[1][2]}.`)
-console.log(`O aluno da posição 3 da lista de alunos é: ${listaDeAlunosEMedias[0][3]}. E sua nota é ${listaDeAlunosEMedias[1][3]}.`)
+console.log(`O aluno da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][0]}. E sua nota é ${listaDeAlunosEMedias[1][0]}.`)
+console.log(`A aluna da posição 2 da lista de alunos é: ${listaDeAlunosEMedias[0][1]}. E sua nota é ${listaDeAlunosEMedias[1][1]}.`)
+console.log(`A aluna da posição 3 da lista de alunos é: ${listaDeAlunosEMedias[0][2]}. E sua nota é ${listaDeAlunosEMedias[1][2]}.`)
+console.log(`O aluno da posição 4 da lista de alunos é: ${listaDeAlunosEMedias[0][3]}. E sua nota é ${listaDeAlunosEMedias[1][3]}.`)
 
 //Com base nessa matriz, qual dos códigos abaixo eu posso utilizar para imprimir a frase “Leonardo tem idade 28”?
 const funcionarios = [["Ana", "Juliana", "Leonardo"],[30, 35, 28]];
