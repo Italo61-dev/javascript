74d9eafc8b868d71432c152f5bd6eef1484d669e
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-10-30 19:16:30 -0300

    Create app.js

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,5 @@
+let frase = document.querySelector('h1');
+frase.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelectorClass( container__texto);
+paragrafo.innerHTML = ',,,,'
