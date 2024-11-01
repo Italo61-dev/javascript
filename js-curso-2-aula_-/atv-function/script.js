422859e4b4b1233faa00cb93edfb4e01fc58dbcc
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-11-01 11:25:02 -0300

    Create script.js

---
+++ js-curso-2-aula_-/atv-function/script.js
@@ -0,0 +1,39 @@
+let nome = 'Italo';
+
+function saudacao(){
+    console.log(`Olá, ${nome}!`)
+}
+
+saudacao();
+
+let numero = 0;
+
+function dobro(numero){
+    return numero * 2;
+}
+
+console.log(dobro(5));
+
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+
+function media(num1, num2, num3){
+    let soma = num1 + num2 + num3;
+    let resultado = soma / 3;
+    return resultado
+}
+
+console.log(media(10, 10, 10));
+
+function numeroMaior(num1, num2){
+    return Math.min(num1,num2);
+}
+
+console.log(numeroMaior(9,11));
+
+function numeroXNumero(a){
+    return a * a;
+}
+
+console.log(numeroXNumero(3))
