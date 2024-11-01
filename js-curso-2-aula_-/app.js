2c7ed7acae90a0050cfca89e98f06901511af5a6
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-11-01 10:42:48 -0300

    Update app.js

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,5 +1,18 @@
-let frase = document.querySelector('h1');
-frase.innerHTML = 'Jogo do número secreto';
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelectorClass( container__texto);
-paragrafo.innerHTML = ',,,,'
+function exibirNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirNaTela('h1', 'Jogo do número secreto');
+exibirNaTela('p', 'Escolha um número de 1 a 10');
+
+
+function verificarChute () {
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio () {
+    return parseInt(Math.random() * 10 + 1);
+}
