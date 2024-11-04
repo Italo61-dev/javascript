257443801c847e01f4f9a15157f1825f08546dc8
Ítalo Nascimento <136705319+Italo61-dev@users.noreply.github.com>
2024-11-04 08:25:05 -0300

    Update script.js

--- js-curso-2-aula_-/atv-function/script.js
+++ js-curso-2-aula_-/atv-function/script.js
@@ -1,39 +1,86 @@
-let nome = 'Italo';
+// let nome = 'Italo';
 
-function saudacao(){
-    console.log(`Olá, ${nome}!`)
-}
+// function saudacao(){
+//     console.log(`Olá, ${nome}!`)
+// }
 
-saudacao();
+// saudacao();
 
-let numero = 0;
+// let numero = 0;
 
-function dobro(numero){
-    return numero * 2;
-}
+// function dobro(numero){
+//     return numero * 2;
+// }
 
-console.log(dobro(5));
+// console.log(dobro(5));
 
-let num1 = 0;
-let num2 = 0;
-let num3 = 0;
+// let num1 = 0;
+// let num2 = 0;
+// let num3 = 0;
 
-function media(num1, num2, num3){
-    let soma = num1 + num2 + num3;
-    let resultado = soma / 3;
-    return resultado
-}
+// function media(num1, num2, num3){
+//     let soma = num1 + num2 + num3;
+//     let resultado = soma / 3;
+//     return resultado
+// }
 
-console.log(media(10, 10, 10));
+// console.log(media(10, 10, 10));
 
-function numeroMaior(num1, num2){
-    return Math.min(num1,num2);
-}
+// function numeroMaior(num1, num2){
+//     return Math.min(num1,num2);
+// }
 
-console.log(numeroMaior(9,11));
+// console.log(numeroMaior(9,11));
 
-function numeroXNumero(a){
-    return a * a;
-}
+// function numeroXNumero(a){
+//     return a * a;
+// }
 
+// console.log(numeroXNumero(3))
+
+// let peso = 0;
+// let altura = 0;
+
+// function imc(peso,altura){
+//     return peso / (altura * altura)
+// }
+
+// console.log(imc(80, 1.84));
+
+// function fatorial(n) {
+//     if (n < 0) {
+//         return "O fatorial não é definido para números negativos.";
+//     } else if (n === 0 || n === 1) {
+//         return 1;
+//     } else {
+//         let resultado = 1;
+//         for (let i = 2; i <= n; i++) {
+//             resultado *= i;
+//         }
+//         return resultado;
+//     }
+// }
+
+// console.log(fatorial(5))
+
+
+// function converterParaReais(dolar){
+//     const cotacaoDolar = 4.80;
+//     return dolar * cotacaoDolar;
+// }
+
+// console.log(converterParaReais(10));
+
+// function tabuada(n1){
+//     console.log(`Tabuada de ${n1}`)
+//     for(let i = 1; i <= 10; i++){
+//         console.log(`${n1} x ${i} = ${n1 * i}`)
+//     }
+// }
+
+// tabuada(3);
+
+<<<<<<< HEAD
 console.log(numeroXNumero(3));
+=======
+>>>>>>> 929e1ddbae6eca37bd676e332ab4ca328acb43d4
